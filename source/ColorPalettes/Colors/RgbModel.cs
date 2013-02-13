53978d5619eebfc005332b4274c658444ae0d9f7
Chris Jansson <chris.janssom@gmail.com>
2013-02-13 19:47:51 +0100

    Added Whitepoint to RgbModel

--- source/ColorPalettes/Colors/RgbModel.cs
+++ source/ColorPalettes/Colors/RgbModel.cs
@@ -4,14 +4,16 @@ namespace ColorPalettes.Colors
 {
     public class RgbModel
     {
-        private RgbModel(Matrix3 workingSpaceMatrix)
+        private RgbModel(WhitePoint whitePoint, Matrix3 workingSpaceMatrix)
         {
+            WhitePoint = whitePoint;
             Matrix = workingSpaceMatrix;
         }
 
+        public readonly WhitePoint WhitePoint;
         public readonly Matrix3 Matrix;
 
-        public static readonly RgbModel AdobeRgbD65 = new RgbModel(new Matrix3(new[,]
+        public static readonly RgbModel AdobeRgbD65 = new RgbModel(WhitePoint.D65, new Matrix3(new[,]
             {
                 {0.5767309,0.1855540,0.1881852},
                 {0.2973769,0.6273491,0.0752741},
