2b7de87cc787c93551bb5760ec0a6cfebc6b1658
Chris Jansson <chris.janssom@gmail.com>
2013-02-15 20:56:14 +0100

    Refactored segmentprovider

---
+++ source/ColorPalettes/Colors/Segment.cs
@@ -0,0 +1,47 @@
+﻿using ColorPalettes.Math;
+
+namespace ColorPalettes.Colors
+{
+    public class Segment
+    {
+        private readonly int _rhoGoesInto;
+        private readonly int _sigmaGoesInto;
+        private readonly int _tauGoesInto;
+
+        public Segment(int rhoGoesInto, int sigmaGoesInto, int tauGoesInto)
+        {
+            _tauGoesInto = tauGoesInto;
+            _sigmaGoesInto = sigmaGoesInto;
+            _rhoGoesInto = rhoGoesInto;
+        }
+
+        public Vector3 Assemble(double rho, double sigma, double tau)
+        {
+            var values = new double[3];
+            values[_rhoGoesInto] = rho;
+            values[_sigmaGoesInto] = sigma;
+            values[_tauGoesInto] = tau;
+
+            return new Vector3(values[0], values[1], values[2]);
+        }
+
+        public Vector3 GetTauVector(Matrix3 matrix3)
+        {
+            return GetCalculationVector(matrix3, _tauGoesInto);
+        }
+
+        public Vector3 GetRhoVector(Matrix3 matrix3)
+        {
+            return GetCalculationVector(matrix3, _rhoGoesInto);
+        }
+
+        private Vector3 GetCalculationVector(Matrix3 matrix3, int column)
+        {
+            var x = matrix3[0, column];
+            var y = matrix3[1, column];
+            var z = matrix3[2, column];
+
+            return new Vector3(x, y, z);
+        }
+    }
+}
--- source/ColorPalettes/Colors/SegmentProvider.cs
+++ source/ColorPalettes/Colors/SegmentProvider.cs
@@ -3,60 +3,21 @@ using ColorPalettes.Math;
 
 namespace ColorPalettes.Colors
 {
-    public class Segment
-    {
-        private readonly int _rho;
-        private readonly int _sigma;
-        private readonly int _tau;
-
-        public Segment(int rho, int sigma, int tau)
-        {
-            _tau = tau;
-            _sigma = sigma;
-            _rho = rho;
-        }
-
-        public Vector3 Assemble(double rho, double sigma, double tau)
-        {
-            var values = new double[3];
-            values[_rho] = rho;
-            values[_sigma] = sigma;
-            values[_tau] = tau;
-
-            return new Vector3(values[0], values[1], values[2]);
-        }
-
-        public Vector3 GetTauVector(Matrix3 matrix3)
-        {
-            return GetCalculationVector(matrix3, _tau);
-        }
-
-        public Vector3 GetRhoVector(Matrix3 matrix3)
-        {
-            return GetCalculationVector(matrix3, _rho);
-        }
-
-        private Vector3 GetCalculationVector(Matrix3 matrix3, int column)
-        {
-            var x = matrix3[0, column];
-            var y = matrix3[1, column];
-            var z = matrix3[2, column];
-
-            return new Vector3(x, y, z);
-        }
-    }
-
     public class SegmentProvider
     {
         private readonly RgbModel _adobeRgbD65;
-        
+
         private double _h0;
         private double _h1;
         private double _h2;
         private double _h3;
         private double _h4;
         private double _h5;
-        
+
+        private const int R = 0;
+        private const int G = 1;
+        private const int B = 2;
+
         public SegmentProvider(RgbModel adobeRgbD65)
         {
             _adobeRgbD65 = adobeRgbD65;
@@ -68,34 +29,34 @@ namespace ColorPalettes.Colors
         {
             if (hue >= _h0 && hue < _h1)
             {
-                return new Segment(1, 2, 0);
+                return new Segment(G, B, R);
             }
-            
+
             if (hue >= _h1 && hue < _h2)
             {
-                return new Segment(0, 2, 1);
+                return new Segment(R, B, G);
             }
-            
+
             if (hue >= _h2 && hue < _h3)
             {
-                return new Segment(2, 0, 1);
+                return new Segment(B, R, G);
             }
-            
+
             if (hue >= _h3 && hue < _h4)
             {
-                return new Segment(1, 0, 2);
+                return new Segment(G, R, B);
             }
-            
+
             if (hue >= _h4 && hue < _h5)
             {
-                return new Segment(0, 1, 2);
+                return new Segment(R, G, B);
             }
-            
+
             if (hue >= _h5 || hue < _h0)
             {
-                return new Segment(2, 1, 0);
+                return new Segment(B, G, R);
             }
-            
+
             throw new NoNullAllowedException();
         }
 
