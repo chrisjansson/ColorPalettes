c7b91465225813a4e030aabeaf67a7a9fa488436
Chris Jansson <chris.janssom@gmail.com>
2013-02-15 21:04:29 +0100

    More refactoring

--- source/ColorPalettes/Colors/SegmentProvider.cs
+++ source/ColorPalettes/Colors/SegmentProvider.cs
@@ -1,12 +1,16 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
 using ColorPalettes.Math;
+using System.Linq;
 
 namespace ColorPalettes.Colors
 {
     public class SegmentProvider
     {
-        private readonly RgbModel _adobeRgbD65;
-
+        private const int R = 0;
+        private const int G = 1;
+        private const int B = 2;
+        
         private double _h0;
         private double _h1;
         private double _h2;
@@ -14,50 +18,26 @@ namespace ColorPalettes.Colors
         private double _h4;
         private double _h5;
 
-        private const int R = 0;
-        private const int G = 1;
-        private const int B = 2;
-
-        public SegmentProvider(RgbModel adobeRgbD65)
+        private readonly RgbModel _rgbModel;
+        private Dictionary<Predicate<double>, Segment> _segmentTable;
+        
+        public SegmentProvider(RgbModel rgbModel)
         {
-            _adobeRgbD65 = adobeRgbD65;
+            _rgbModel = rgbModel;
 
             CalculateSegments();
+            CreateSegmentTable();
         }
 
         public Segment GetSegmentForHue(double hue)
         {
-            if (hue >= _h0 && hue < _h1)
+            var segment = _segmentTable.SingleOrDefault(x => x.Key(hue)).Value;
+            if (segment == null)
             {
-                return new Segment(G, B, R);
+                throw new NotImplementedException("Doesn't match any defined segments, this is only implemented for AdobeRGB");
             }
 
-            if (hue >= _h1 && hue < _h2)
-            {
-                return new Segment(R, B, G);
-            }
-
-            if (hue >= _h2 && hue < _h3)
-            {
-                return new Segment(B, R, G);
-            }
-
-            if (hue >= _h3 && hue < _h4)
-            {
-                return new Segment(G, R, B);
-            }
-
-            if (hue >= _h4 && hue < _h5)
-            {
-                return new Segment(R, G, B);
-            }
-
-            if (hue >= _h5 || hue < _h0)
-            {
-                return new Segment(B, G, R);
-            }
-
-            throw new NoNullAllowedException();
+            return segment;
         }
 
         private void CalculateSegments()
@@ -75,11 +55,24 @@ namespace ColorPalettes.Colors
             var colorConverter = new ColorConverter();
 
             var rgb = new Vector3(r, g, b);
-            var xyz = colorConverter.ConvertToXyz(rgb, _adobeRgbD65);
-            var luv = colorConverter.ConvertToLuv(xyz, _adobeRgbD65.WhitePoint);
+            var xyz = colorConverter.ConvertToXyz(rgb, _rgbModel);
+            var luv = colorConverter.ConvertToLuv(xyz, _rgbModel.WhitePoint);
             var lch = colorConverter.ConvertToLchuv(luv);
 
             return lch.H;
+        }
+
+        private void CreateSegmentTable()
+        {
+            _segmentTable = new Dictionary<Predicate<double>, Segment>
+                {
+                    {hue => hue >= _h0 && hue < _h1, new Segment(G, B, R)},
+                    {hue => hue >= _h1 && hue < _h2, new Segment(R, B, G)}, 
+                    {hue => hue >= _h2 && hue < _h3, new Segment(B, R, G)},
+                    {hue => hue >= _h3 && hue < _h4, new Segment(G, R, B)},
+                    {hue => hue >= _h4 && hue < _h5, new Segment(R, G, B)},
+                    {hue => hue >= _h5 || hue < _h0, new Segment(B, G, R)},
+                };
         }
     }
 }
