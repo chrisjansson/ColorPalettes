5587b990963ca9afa4749b51a82d1febceb44490
Chris Jansson <chris.janssom@gmail.com>
2013-02-27 19:30:58 +0100

    Parameters object for the general color generation

---
+++ source/ColorPalettes/Colors/CalculationParameters.cs
@@ -0,0 +1,20 @@
+﻿namespace ColorPalettes.Colors
+{
+    public class CalculationParameters
+    {
+        public CalculationParameters(int numberOfColors, double hue, double contrast, double saturation, double brightness)
+        {
+            Brightness = brightness;
+            Saturation = saturation;
+            Contrast = contrast;
+            Hue = hue;
+            NumberOfColors = numberOfColors;
+        }
+
+        public int NumberOfColors { get; private set; }
+        public double Hue { get; private set; }
+        public double Contrast { get; private set; }
+        public double Saturation { get; private set; }
+        public double Brightness { get; private set; }
+    }
+}
